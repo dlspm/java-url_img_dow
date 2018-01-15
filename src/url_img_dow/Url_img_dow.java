f7d7ca2c1c735ba1cac6049d90ff1f0df3d7a462
dlspm <q0978298512@gmail.com>
2018-01-15 21:47:50 +0800

    test click JTextField

--- src/url_img_dow/Url_img_dow.java
+++ src/url_img_dow/Url_img_dow.java
@@ -19,11 +19,15 @@ import java.awt.MediaTracker;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
@@ -31,21 +35,26 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.lang.model.util.Elements;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
 import javax.swing.tree.DefaultMutableTreeNode;
 
 public class Url_img_dow extends JFrame  implements ActionListener  {
 
-    private JPanel p, jPanel1, pp;
-    JButton jButton ;
+    private JPanel p, jPanel1, pp, ppp;
+    JButton Btndow, Btnimgpath ;
+    JTextField theTextField;
     Container c = this.getContentPane();
     String strimg;
             ConcurrentHashMap<String, String> dict = new ConcurrentHashMap<String, String>();
@@ -131,6 +140,23 @@ public class Url_img_dow extends JFrame  implements ActionListener  {
                 p.repaint();
             }
         });
+        
+        ppp = new JPanel();
+        ppp.setLayout(new FlowLayout());
+        Btnimgpath = new JButton("輸入網址");
+        ppp.add(Btnimgpath);
+        Btnimgpath.addActionListener(this);
+        theTextField = new JTextField(20);
+        ppp.add(theTextField);
+        
+        
+        c.add(ppp, BorderLayout.NORTH);
+        
+        
+        
+        
+        
+        
         c.add(tree, BorderLayout.WEST);
 
         p = new JPanel();
@@ -139,10 +165,10 @@ public class Url_img_dow extends JFrame  implements ActionListener  {
         
         pp = new JPanel();
         pp.setLayout(new FlowLayout());
-        jButton = new JButton("下載");
-        jButton.addActionListener(this);
+        Btndow = new JButton("下載");
+        Btndow.addActionListener(this);
         
-        pp.add(jButton, BorderLayout.SOUTH);
+        pp.add(Btndow, BorderLayout.SOUTH);
 //        pp.repaint();
         c.add(pp, BorderLayout.SOUTH);
 
@@ -157,14 +183,149 @@ public class Url_img_dow extends JFrame  implements ActionListener  {
     }
     
     public void actionPerformed(ActionEvent e){
-        if (e.getSource() == jButton) {try {
+        if (e.getSource() == Btndow) {
+            try {
             //button_open不要加引号
-            Dowimg();
+                Dowimg();
             } catch (Exception ex) {
                 Logger.getLogger(Url_img_dow.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } 
+        }else if(e.getSource() == Btnimgpath){
+            try {
+                
+                String url = theTextField.getText();
+                
+                System.out.println(theTextField.getText());
+                
+//                String encoding = "gb2312";
+//                //1.根据网络和页面的编码集 抓取网页的源代码
+//                String htmlResouce = GetHtmlResouceByURL(url, encoding); //GetHtmlResouceByURL() 會 return String 整個 html/XML  回來
+//                //System.out.println(htmlResouce);
+//
+//                //2.解析网页的源代码 jsoup jar包
+//                Document document = Jsoup.parse(htmlResouce, "UTF-8");
+//                System.out.println(document);
+//                Get_instagram_imgurl(document,url);
+                
+            } catch (Exception ex) {
+                Logger.getLogger(Url_img_dow.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
+    
+    public void treefile(){
+    
+    
+    }
+    
+//    
+//    public static void Get_instagram_imgurl(Document document, String url) {
+//        //meta, content
+//        int n = 0;
+//        Elements elements = document.getElementsByTag("a");
+//        // 回傳元素 (element) 指定標籤的後代集合物件 (object) 。
+//
+//        //輸出圖片路徑到 imgpath.txt
+//        File fpath = new File("imgpath.txt");
+////        FileOutputStream o = new FileOutputStream(fpath);
+//
+//        try {
+//
+//            FileOutputStream out = new FileOutputStream(fpath);
+//
+//            for (Element element : elements) {
+//                String imgSrc = element.attr("href"); //获取src属性的值(attr代表某個元素的屬性)
+//                imgSrc = url.substring(0, 25) + imgSrc;
+//                System.out.println("attr:" + imgSrc);
+//                //下载到本地文件夹中
+//                if (imgSrc.length() > 29) {
+//                    Get_instagram_imgdown(imgSrc, imgSrc.substring(29, imgSrc.length() - 1), out);
+//                }
+//
+//                //            if (n == 1) {
+//                //                break;
+//                //            } else {
+//                //                n++;
+//                //            }
+//            }
+//
+//            out.close();
+//
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//
+//    }
+//    
+//    public static void Get_instagram_imgdown(String imgsrc, String filename, FileOutputStream out) throws IOException {
+//
+//        String htmlResouce = GetHtmlResouceByURL(imgsrc, "gb2312");
+//        Document document = Jsoup.parse(htmlResouce, "UTF-8");
+//
+//        Elements elements = document.getElementsByTag("meta");
+////             回傳元素 (element) 指定標籤的後代集合物件 (object) 。
+//
+//        for (Element element : elements) {
+//            String imgSrc = element.attr("content"); //获取src属性的值(attr代表某個元素的屬性) 
+//            if (imgSrc.length() > 18) {
+//                if ("https://instagram.".equals(imgSrc.substring(0, 18))) {
+//                    System.out.println("attr:" + imgSrc);
+//
+//                    byte[] bimgpath = (imgSrc + "\n").getBytes();
+//                    out.write(bimgpath);
+//                    downImgs(imgSrc, filename);
+//                    break;
+//                }
+//            }
+//        }
+//
+//    }
+//    
+//    public static String GetHtmlResouceByURL(String url, String encoding) {
+//
+//        // 建立容器存储网页源代码
+//        StringBuffer buffer = new StringBuffer();
+//        URL urlobj = null;
+//        URLConnection uc = null;
+//        InputStreamReader isr = null;
+//        BufferedReader input = null;
+//        try {
+//            //建立网络连接
+//            urlobj = new URL(url);
+//            //打开网络连接
+//            uc = urlobj.openConnection();
+//            //建立网络输入流
+//            isr = new InputStreamReader(uc.getInputStream(), encoding);
+//            //建立缓冲流读输入的数据
+//            input = new BufferedReader(isr);
+//
+//            //循环遍历数据
+//            String line = null;
+//            while ((line = input.readLine()) != null) {
+//                //添加换行
+//                buffer.append(line + "\n");
+//            }
+//
+//        } catch (Exception e) {
+//            // TODO Auto-generated catch block
+//
+//            e.printStackTrace();
+//            System.out.println("連接源代碼失敗");
+//        } finally {
+//            try {
+//                if (isr != null) {
+//                    isr.close();
+//                }
+//                input.close();
+//            } catch (IOException e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//                System.out.println("關閉失敗");
+//            }
+//        }
+//
+//        return buffer.toString();
+//    }
     
     public Image reduceimg(ImageIcon icon){
         
@@ -250,12 +411,12 @@ public class Url_img_dow extends JFrame  implements ActionListener  {
             fos.write(r);
         }
         fos.close();
-        System.out.println(strimg + "Down is OK");
+        System.out.println( "Dow/" + strimg + "Down is OK");
 
     }
     
     
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, IOException {
         new Url_img_dow("Test Swing Jtree v2").init();
     }
     
